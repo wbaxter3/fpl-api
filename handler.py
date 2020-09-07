48c7c29e67bac8bbee3d65c48adb78c6aed44a84
William Baxter <wbaxter3@gmail.com>
2020-09-07 00:13:54 -0400

    init

---
+++ handler.py
@@ -0,0 +1,49 @@
+import json
+from fpl import FPL
+import aiohttp
+import asyncio
+
+async def main():
+
+    session = aiohttp.ClientSession()
+    fpl = FPL(session)
+    players = await fpl.get_players()
+    await session.close()
+
+    body = {
+        "players":players
+    }
+
+
+    response = {
+        "statusCode":200,
+        "body":players
+    }
+    print(response)
+    return response
+
+
+# def hello(event, context):
+#     body = {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "input": event
+#     }
+#
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
+#async withaiohttp.ClientSession as session:
+#     return response
+#
+#     # Use this code if you don't use the http event with the LAMBDA-PROXY
+#     # integration
+#     """
+#     return {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "event": event
+#     }
+#     """
+if __name__ == "__main__":
+
+    asyncio.run(main())
