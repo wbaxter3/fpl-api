a816030d2517dea23860f52e55e8b82d3f72d4ce
William Baxter <wbaxter3@gmail.com>
2020-09-15 21:07:12 -0400

    fguyg

--- handler.py
+++ handler.py
@@ -3,33 +3,30 @@ import requests
 import boto3
 
 from dynamodb_json import json_util
-#def main(event, context):
-def main():
+def main(event, context):
+#def main():
 
 
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    # if event['year'] == "2014":
-    #     tablename = "2014_whole_season_data"
-    # elif event['year'] == "2015":
-    #     tablename = "2015_whole_season_data"
-    # elif event['year'] == "2016":
-    #     tablename = "2016_whole_season_data"
-    # elif event['year'] == "2017":
-    #     tablename = "2017_whole_season_data"
-    # elif event['year'] == "2018":
-    #     tablename = "2018_whole_season_data"
-    # elif event['year'] == "2019":
-    #     tablename = "2019_whole_season_data"
-    tablename="2019_whole_season_data"
+    if event['year'] == "2014":
+        tablename = "2014_whole_season_data"
+    elif event['year'] == "2015":
+        tablename = "2015_whole_season_data"
+    elif event['year'] == "2016":
+        tablename = "2016_whole_season_data"
+    elif event['year'] == "2017":
+        tablename = "2017_whole_season_data"
+    elif event['year'] == "2018":
+        tablename = "2018_whole_season_data"
+    elif event['year'] == "2019":
+        tablename = "2019_whole_season_data"
+    #tablename="2019_whole_season_data"
     table = dynamodb.Table(tablename)
 
-    raw_player_data = table.scan()
+    player_data = table.scan()
+    #dynamodb_json = json_util.dumps(player_data)
+    #print(dynamodb_json)
 
-    player_data = {}
-    for player in raw_player_data['Items']:
-        player_data['player_name'] = player['player_name'],
-        player_data['games_played'] = player['games']
-        print(player['player_name'])
     #url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
     #r = requests.get(url)
     # json = r.json()
@@ -40,10 +37,33 @@ def main():
     #     "input":event
     # }
 
+    players = []
+    for item in player_data['Items']:
+        player = {}
+        player['player_name'] = item['player_name']
+        player['goals_season'] = item['goals_season']
+        player['assists_season'] = item['assists_season']
+        player['games'] = item['games']
+        player['key_passes_Season'] = item['key_passes_Season']
+        player['npg_season'] = item['npg_season']
+        player['npxG_season'] = item['npxG_season']
+        player['position'] = item['position']
+        player['red_cards'] = item['red_cards']
+        player['shots_season'] = item['shots_season']
+        player['team_title'] = item['team_title']
+        player['time'] = item['time']
+        player['xA_season'] = item['xA_season']
+        player['xGBuildup'] = item['xGBuildup']
+        player['xGChain'] = item['xGChain']
+        player['xG_season'] = item['xG_season']
+        player['yellow_cards'] = item['yellow_cards']
+        players.append(player)
+    data = {"players":players}
+
 
     response = {
         "statusCode":200,
-        "body": json.dumps(dynamodb_json)
+        "body": data
     }
     #print(players)
     return response
